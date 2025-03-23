803e5de80cf7b4117c74874b9ce28cee64b199c0
LiamM <liam118paw@gmail.com>
2025-03-22 23:06:19 -0400

    (cleanup) gets rid of unused "using ...." lines.

--- Constants.cs
+++ Constants.cs
@@ -1,5 +1,4 @@
-﻿using UnityEngine;
-namespace GorillaTrials
+﻿namespace GorillaTrials
 {
     public class Constants
     {
--- Plugin.cs
+++ Plugin.cs
@@ -1,7 +1,5 @@
 ﻿using BepInEx;
 using System.IO;
-using System.Linq;
-using System.Reflection;
 using UnityEngine;
 using System.Collections;
 
