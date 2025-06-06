24816ca87882f28b2224a5033fce78fbe2a0da15
Lapis <47952866+LapisGit@users.noreply.github.com>
2025-06-06 18:05:13 -0400

    make better way of loading challenges

--- LoadChallenges.cs
+++ LoadChallenges.cs
@@ -8,41 +8,36 @@ namespace GorillaTrials
 {
     public class LoadChallenges : BaseUnityPlugin
     {
-        public static GameObject buttonObject { get; private set; }
-
+        public static GameObject trialUIObject;
+        public static AssetBundle bundle = AssetBundle.LoadFromFile(tempPath);
+        public static string tempPath = Path.Combine(Path.GetTempPath(), Constants.AssetBundleName);
         public static void LoadAssetBundle()
         {
-            Debug.Log("Loading AssetBundle...");
+            Debug.Log("Loading Trials...");
 
             string resourceName = Assembly.GetExecutingAssembly().GetManifestResourceNames()
                                           .FirstOrDefault(name => name.EndsWith(Constants.AssetBundleName));
-
-            string tempPath = Path.Combine(Path.GetTempPath(), Constants.AssetBundleName);
 
             using (Stream resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
             using (FileStream fileStream = new FileStream(tempPath, FileMode.Create, FileAccess.Write))
             {
                 resourceStream.CopyTo(fileStream);
             }
-
-            AssetBundle bundle = AssetBundle.LoadFromFile(tempPath);
+            
             if (bundle == null)
             {
                 Debug.Log("Failed to load AssetBundle!");
                 return;
             }
-
-            // challenge 1
             
-            GameObject challenge1 = bundle.LoadAsset<GameObject>("ForestBoxTrial1");
-            if (challenge1 == null)
-            {
-                Debug.Log("Failed to load prefab from AssetBundle!");
-                return;
-            }
+            CreateChallenge("Test Trial", "testtrial", new Vector3(0,0,0));
+        }
 
-            Vector3 spawnPositionchallenge1 = new Vector3(-66.5785f, 11.8871f, -82.7937f);
-            Plugin.loadedPrefab = Instantiate(challenge1, spawnPositionchallenge1, Quaternion.identity);
+        public static void CreateChallenge(string triallongname, string trialservername, Vector3 position)
+        {
+            trialUIObject = bundle.LoadAsset<GameObject>("Trial");
+            trialUIObject.name = triallongname;
+            trialUIObject.transform.position = position;
         }
         
         }
