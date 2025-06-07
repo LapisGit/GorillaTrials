5c939599047d76b9334429f646f67002db287165
Lapis <47952866+LapisGit@users.noreply.github.com>
2025-06-07 19:30:47 -0400

    a

--- CustomProps.cs
+++ CustomProps.cs
@@ -5,11 +5,6 @@ namespace GorillaTrials
 
     public class CustomProps
     {
-        [JsonProperty("PBs")]
-        public List<Dictionary<string, float>> PBs { get; set; } = new List<Dictionary<string, float>>();
-        public void AddPB(string trialName, float time)
-        {
-            PBs.Add(new Dictionary<string, float> { { trialName, time } });
-        }
+        //idfk
     }
 }
