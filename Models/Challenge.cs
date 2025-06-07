ef91b4b65590f903016835ae710c34ba3cf825f1
Lapis <47952866+LapisGit@users.noreply.github.com>
2025-06-06 20:39:58 -0400

    add base for challenge model

---
+++ Models/Challenge.cs
@@ -0,0 +1,13 @@
+namespace GorillaTrials.Models
+{
+    public enum TrialType
+    {
+        Box,
+        Zone
+    }
+    
+    public class Challenge
+    {
+        public string challengeName;
+    }
+}
