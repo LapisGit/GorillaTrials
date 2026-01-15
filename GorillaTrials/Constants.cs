85bc729f41e6bcefd243c6d1fc9c0356b0ae8b45
Lapis <47952866+LapisGit@users.noreply.github.com>
2026-01-14 21:32:44 -0500

    bump ver 2/2

--- GorillaTrials/Constants.cs
+++ GorillaTrials/Constants.cs
@@ -6,7 +6,7 @@
 
         public const string Name = "GorillaTrials";
 
-        public const string Version = "1.4.0";
+        public const string Version = "1.5.0";
 
         public const float NetworkSetInterval = 0.25f;
 
